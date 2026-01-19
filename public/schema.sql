0568c9bc56b75a71c96c4cdbc04b630c1552a60b
thomaslucking <telucking@gmail.com>
2026-01-19 12:01:05 +0100

    feat: created database with sqlite3 closes #2

---
+++ public/schema.sql
@@ -0,0 +1,18 @@
+
+create table users (
+    id integer primary key autoincrement,
+    name varchar(35) not null check(length(name) <= 35),
+    email text not null unique,
+    password text not null
+);
+
+
+create table posts (
+    id integer primary key autoincrement,
+    title text not null,
+    image text not null,
+    content text not null,
+    created_at datetime default current_timestamp,
+    user_id integer not null,
+    foreign key (user_id) references users(id) on delete cascade
+);
