ac0505dd4cbbae3d7d7e41edd226d822b2c69f36
thomaslucking <telucking@gmail.com>
2026-02-02 12:55:57 +0100

    feat: added logout functionality closes #4

--- src/Services/Authorization.php
+++ src/Services/Authorization.php
@@ -19,11 +19,13 @@ class Authorization
     //     return self::isLoggedIn() ? (int)$_SESSION['user_id'] : null;
     // }
 
-    public static function canAccessPost(int $postId, UserModel $model): bool
+    public static function canAccessPost(int $postId): bool
     {
         if (!self::isLoggedIn()) {
             return false;
         }
+        $pdo = Database::getConnection();
+        $model = new UserModel($pdo);
 
         $userId = (int)$_SESSION['user_id'];
         return $model->linkUserWithPost($userId, $postId);
